17e2926319b6364968a5aafc15d04aa5116417d7
João Neto <joaopintoneto@gmail.com>
2022-04-15 21:24:53 -0300

    add behaviours

---
+++ src/lib/behaviours.ts
@@ -0,0 +1,60 @@
+import createPubSub from './pub-sub';
+import { Router } from './router';
+
+const { publish, subscribe, crearSubscribers } = createPubSub();
+
+Router.onChangeStart(() => {
+  crearSubscribers();
+});
+
+export const listen =
+  (eventName: string) => (element: Element, listener: (...args: any) => void) => {
+    element.addEventListener(eventName, listener);
+    return element;
+  };
+
+export const click = listen('click');
+
+export const each =
+  (selector: string) =>
+  (initialData: any, mapFn: (item: any, i?: number) => Element): Element[] => {
+    subscribe(`${selector}:behaviour:remove`, (dataId: string) => {
+      const element = document.querySelector(selector);
+      if (element) {
+        const child = element.querySelector(`[data-id="${dataId}"]`);
+        if (child) {
+          element.removeChild(child);
+        }
+      }
+    });
+
+    subscribe(`${selector}:behaviour:add`, (item: any) => {
+      const element = document.querySelector(selector);
+      if (element) {
+        element.appendChild(mapFn(item, element.children.length));
+      }
+    });
+
+    subscribe(`${selector}:behaviour:edit`, ({ dataId, ...item }: any) => {
+      const element = document.querySelector(selector);
+      if (element) {
+        const oldEl = element.querySelector(`[data-id="${dataId}"]`);
+        if (oldEl) {
+          const newEl = mapFn({ dataId, ...item }, element.children.length);
+          if (newEl) {
+            element.replaceChild(newEl, oldEl);
+          }
+        }
+      }
+    });
+
+    return initialData.map(mapFn);
+  };
+
+export const add = (selector: string) => (data: any) => publish(`${selector}:behaviour:add`, data);
+
+export const edit = (selector: string) => (data: any) =>
+  publish(`${selector}:behaviour:edit`, data);
+
+export const remove = (selector: string) => (index: string) =>
+  publish(`${selector}:behaviour:remove`, index);
